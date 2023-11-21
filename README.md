c8097e88866edafddd7bf728130baf43280ba0cb
ElSera99 <stbugim@gmail.com>
2023-11-21 00:01:38 -0600

    CPU main ISAs class script for implementation and readme update

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # isa_python_implementation
-A simple python script in order to imitate ISA an CPU functionality.
+As part as the [Computer Science Bootcamp]((https://codigofacilito.com/bootcamps/ciencias-computacionales/dashboard)) 
+from [Codigo Facilito](https://codigofacilito.com/)
+, in the class **Machines**, we learned how a computer internally process
+the information and instructions.
+
+The purpose of this repository is to create simulation that represents
+the main functionality and **ISA** of a simple computer.
+
+![full_execution](./images/full_execution.png)
+
+## Architecture
+Taking as inspiration the **Von Newman** architecture has the following main components: 
+![Von Neumann](./images/von_neumann.png)
+
+- **ALU:** Arithmetic Logic Unit
+- **CPU:** Central Process Unit 
+- **RAM:** Random Access Memory
+- **IP:** Instruction Pointer - Small register(number) in the CPU, 
+it points the instruction of the program in the RAM.
+- **SP:** Stack Pointer - Indicates where data is going to be stored
+
+The CPU retrieves small chunks of instructions from the RAM memory 
+that have been previously loaded from the hard drive.
+
+These instructions are read from the memory by the **IP**,
+loaded, and executed by the **ALU**
+
+After the **ALU** executes the corresponding arithmetic and 
+logic operations the results can be stored back to the memory.
+
+In order to store or retrieve data from the memory, the **SP**
+can point where the data needs to be retrieved from or stored to.
+
+The **ISA**(Instruction Set Architecture) is a set of instructions 
+that define what kind of operations can be performed on the hardware.
+
+## Registers
+Registers are tiny spaces of memory that can be used to read and write values
+that usually represent addresses in the memory. 
+
+The size of a register determines how big the data can be stored
+and how many memory registers can access
+![Registers](./images/registers.png)
+
+## Code Example
+As previously stated, the purpose of this repository is to simulate
+the basic instructions of a CPU:
+
+These instructions are the following:
+```python
+"""
+Recreate the basic operations of a CPU, instructions occur as follows:
+(<operation>, [comparison], <registry a>, <registry b>, <registry c>)
+
+# Add two registries and store into another registry
+('ADD', 'Ra', 'Rb', 'Rc')
+# Subtract Rb from Ra and store into another registry
+('SUB', 'Ra', 'Rb', 'Rc')
+# Multiply two registries and store into another registry
+('MUL', 'Ra', 'Rb', 'Rc')
+# Divide two registries and store into another registry
+('DIV', 'Ra', 'Rb', 'Rc')
+# Add one to the registry and store into the SAME registry
+('INC', 'Ra')
+# Subtract one to the registry and store into the SAME registry
+('DEC', 'Ra')
+# Compare two registries according to the desired operation
+('CMP', 'op', 'Ra', 'Rb', 'Rc') # op = {<,>,<=, >=, ==, !=}
+# Load from RAM memory
+('LOAD', 'Rs', 'Rd', offset) # offset -> direction where to store data
+# Save to RAM memory
+('STORE', 'Rs', 'Rd', offset) # offset -> direction where to store data
+# Jump from one registry to another
+('JMP', Rd, offset)
+# End the program
+('HALT')
+
+"""
+```
+Implementation must be from a CPU class as abstraction and must have a method for every operation stated.
+Must have a structure for representing the registers of the CPU and a way to access the memory.
+
+Finally, must be able to store and retrieve data from the memory.
---
+++ isas.py
@@ -0,0 +1,120 @@
+class ISA:
+    """ This class represents the set of instructions that the CPU can execute """
+    def __init__(self):
+        # Registers of CPU: 8
+        # Two extra registers for IP and SP
+        self.registers = {"Ra": 0,
+                          "Rb": 0,
+                          "Rc": 0,
+                          "Rd": 0,
+                          "Re": 0,
+                          "Rf": 0,
+                          "Rg": 0,
+                          "Rh": 0,
+                          "IP": 0,  # Instruction pointer indicates the number of instruction that is being executed
+                          "SP": 0  # Stack pointer indicates the stack pointer where the data is going to be stored
+                          }
+        # Simulation of the memory in a computer
+        self.memory = [0] * 1024
+
+    def ADD(self, reg1, reg2, reg3):
+        """ Add to numbers stated in register 1 and register 2 and store in register 3 """
+        self.registers[reg3] = self.registers[reg1] + self.registers[reg2]
+        self.registers['IP'] += 1
+        return self.registers[reg3]
+
+    def SUB(self, reg1, reg2, reg3):
+        self.registers[reg3] = self.registers[reg1] - self.registers[reg2]
+        self.registers['IP'] += 1
+        return self.registers[reg3]
+
+    def MUL(self, reg1, reg2, reg3):
+        self.registers[reg3] = self.registers[reg1] * self.registers[reg2]
+        self.registers['IP'] += 1
+        return self.registers[reg3]
+
+    def DIV(self, reg1, reg2, reg3):
+        self.registers[reg3] = self.registers[reg1] / self.registers[reg2]
+        self.registers['IP'] += 1
+        return self.registers[reg3]
+
+    def INC(self, reg1):
+        """ Increase in one the number in the register and save into the same register """
+        self.registers[reg1] = self.registers[reg1] + 1
+        self.registers['IP'] += 1
+        return self.registers[reg1]
+
+    def DEC(self, reg1):
+        """ Decrease in one the number in the register and save into the same register """
+        self.registers[reg1] = self.registers[reg1] - 1
+        self.registers['IP'] += 1
+        return self.registers[reg1]
+
+    def CMP(self, op, reg1, reg2, reg3):
+        """ Comparison of two numbers and store result into register 3 """
+        operators = {1: self.registers[reg1] < self.registers[reg2],
+                     2: self.registers[reg1] > self.registers[reg2],
+                     3: self.registers[reg1] <= self.registers[reg2],
+                     4: self.registers[reg1] >= self.registers[reg2],
+                     5: self.registers[reg1] == self.registers[reg2],
+                     6: self.registers[reg1] != self.registers[reg2]
+                     }
+        self.registers['IP'] += 1
+        self.registers[reg3] = operators[op]
+        return self.registers[reg3]
+
+    def CONST(self, value, reg1):
+        """ Define a constant that can be operated with the previously defined methods """
+        self.registers[reg1] = value
+        self.registers['IP'] += 1
+        return self.registers[reg1]
+
+    # reg_store = CPU register to store, reg_data = RAM register to load from
+    def LOAD(self, reg_store, reg_data, offset):
+        """ Load data from a sector of the memory, will load bit stream from the sectors of the memory """
+        self.registers[reg_store] = self.memory[reg_data:reg_data+offset]
+        self.registers['IP'] += 1
+        return self.registers[reg_store]
+
+    # reg_store = CPU register where data is temporarily stored, reg_data = RAM register to save data to
+    def STORE(self, reg_store, reg_data, offset):
+        """ Will store a stream of bits into the memory """
+        self.memory[reg_data] = self.registers[reg_store]
+        self.registers['IP'] += 1
+        return self.memory[reg_data]
+
+    def JMP(self, reg_data, offset):
+        """ Will jump from one sector of the memory to another """
+        self.registers['SP'] = self.DECODE(self.memory[reg_data:reg_data+offset])
+        return self.registers['SP']
+
+    def HALT(self):
+        """ Will stop execution of program """
+        print(self.registers['IP'],'End of Program')
+        exit()
+
+    def ENCODE(self, value):
+        """ Take an integer and transform into a stream of bits """
+        result = []
+        while value // 2 != 0:
+            result.append(value % 2)
+            value //= 2
+            if value == 1:
+                result.append(1)
+
+        return result[::-1]
+
+    def DECODE(self, value):
+        """ Take a stream of bits and transform into an integer """
+        result = 0
+        power = len(value) - 1
+        for element in value:
+            result += element * (2 ** power)
+            power -= 1
+
+        return result
+
+
+if __name__ == '__main__':
+    # My architecture instance
+    my_architecture = ISA()
